9c124f96d7ac761a6a179424d6e1a62a0945a909
Листик <listik@mac-alex.local>
2021-12-27 22:51:06 +0300

    Init

---
+++ js_1/script.js
@@ -0,0 +1,28 @@
+function showMessage () {
+    let name = prompt("Как тебя зовут?");
+    alert("Привет, " + name + "!");
+}
+
+function sum(a, b) {
+    a = prompt("Первое число");
+    b =  prompt("Второе число");
+    alert(Number(a) + Number(b));
+}
+
+function sub(a, b) {
+    a = prompt("Первое число");
+    b =  prompt("Второе число");
+    alert(a - b);
+}
+
+function div(a, b) {
+    a = prompt("Первое число");
+    b =  prompt("Второе число");
+    alert(a / b);
+}
+
+function mult(a, b) {
+    a = prompt("Первое число");
+    b =  prompt("Второе число");
+    alert(a * b);
+}
