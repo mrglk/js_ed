d8a37b0b24e7d7b356e53740994f44555a5ee184
Листик <listik@mac-alex.local>
2022-01-13 20:40:38 +0300

    New

--- js2_calc/script.js
+++ js2_calc/script.js
@@ -1,43 +1,58 @@
-let showMessage = () => {
-    let name = document.getElementById('name').value;
-    let nameResult = document.getElementById('nameResult');
-    return nameResult.textContent = "Привет, " + name + "!";
-}
+// приветствие
 
-let sum = (a, b) => {
+const showMessage = () => {
+    let input = document.getElementById('name');
+    let nameResult = document.getElementById('nameResult');
+    nameResult.textContent = "Привет, " + input.value + "!";
+    input.value = "";
+};
+
+// калькулятор
+
+const sum = (a, b) => {
     a = document.getElementById('sum1').value;
     b = document.getElementById('sum2').value;
     let result = Number(a) + Number(b);
     let sumResult = document.getElementById('sumResult');
     return sumResult.textContent = result;
-}
+};
 
-let sub = (a, b) => {
+const sub = (a, b) => {
     a = document.getElementById('sub1').value;
     b = document.getElementById('sub2').value;
     let result = Number(a) - Number(b);
     let subResult = document.getElementById('subResult');
     return subResult.textContent = result;
-}
+};
 
-let div = (a, b) => {
+const div = (a, b) => {
     a = document.getElementById('div1').value;
     b = document.getElementById('div2').value;
     let result = Number(a) / Number(b);
     let divResult = document.getElementById('divResult');
     return divResult.textContent = result;
-}
+};
 
-let mult = (a, b) => {
+const mult = (a, b) => {
     a = document.getElementById('mult1').value;
     b = document.getElementById('mult2').value;
     let result = Number(a) * Number(b);
     let multResult = document.getElementById('multResult');
     return multResult.textContent = result;
-}
+};
 
-let divColor = () => {
-    let div = document.getElementsByTagName('div');
-    console.log(div);
-    
-}
+// дивы
+
+const divColor = sender => sender.classList.add('column_color');
+
+// галерея
+
+const frw = () => {
+    let img = document.querySelector('.photo');
+    img.src = "img/dog.jpeg";
+};
+
+const bck = () => {
+    let img = document.querySelector('.photo');
+    img.src = "img/cat.jpeg";
+};
