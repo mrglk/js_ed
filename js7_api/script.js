82a43eb38f1f79f93ba26d32802e222f502ab1d2
mrglk <mariagalko.info@gmail.com>
2022-03-06 22:20:07 +0300

    Init api

---
+++ js7_api/script.js
@@ -0,0 +1,11 @@
+// dedCR1EV1DQLV0srzNeGaRXMKD0o8ELOwrXhMQaY
+
+// let key = https://api.nasa.gov/planetary/apod?api_key=dedCR1EV1DQLV0srzNeGaRXMKD0o8ELOwrXhMQaY;
+
+
+// fetch("https://api.github.com/users/mrglk")
+// .then(response => response.json())
+// .then(user => console.log(user))
+// .catch(error => console.log(error))
+
+
