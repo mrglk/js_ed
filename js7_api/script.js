ed202f2252cc6fc3024b199d2ea1c989022160c5
mrglk <mariagalko.info@gmail.com>
2022-03-07 00:21:30 +0300

    Finish gif api

--- js7_api/script.js
+++ js7_api/script.js
@@ -1,11 +1,25 @@
-// dedCR1EV1DQLV0srzNeGaRXMKD0o8ELOwrXhMQaY
+let key = "SSTJo9QJnNjqIOhkB0INcvjv0XXYmq5I";
+const button = document.querySelector("button");
+const gifContainer = document.querySelector(".gif__container");
 
-// let key = https://api.nasa.gov/planetary/apod?api_key=dedCR1EV1DQLV0srzNeGaRXMKD0o8ELOwrXhMQaY;
+button.addEventListener("click", function () {
+    const input = document.querySelector("input");
 
+   if(input.value == "") {
+       return
+   }
 
-// fetch("https://api.github.com/users/mrglk")
-// .then(response => response.json())
-// .then(user => console.log(user))
-// .catch(error => console.log(error))
+   fetch("https://api.giphy.com/v1/gifs/search?api_key=" + key + "&q=" + input.value + "&limit=25&offset=0&rating=g&lang=en")
+   .then(response => response.json())
+   .then(data => {
+       for (let i = 0; i < data.data.length; i++) {
+        const img = document.createElement("img");
+        img.src = data.data[i].images.original.url;;
+        gifContainer.appendChild(img);
+       }
+   })
+   .catch(error => console.log(error))
+
+})
 
 
