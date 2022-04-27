02e3208f76d2dd4198ea33eb6c3430e49be501d4
Мария Романович <mariagalko.info@gmail.com>
2022-04-28 00:27:56 +0300

    Finish react3

--- react3_tariff/src/App.js
+++ react3_tariff/src/App.js
@@ -1,16 +1,27 @@
 import styles from './App.css';
 import Tariff from './components/Tariff';
 import Tariffs from './tariffs.json';
-// import blue from "./components/blue.css";
-// import green from "./components/green.css";
+import blue from "./components/blue.css";
+import green from "./components/green.css";
+import red from "./components/red.css";
+import black from "./components/black.css";
 
 function App() {
+  const themes = [blue, green, red, black]
   return (
     <div className={styles.App}>
       {
-      Tariffs.map((tariff) =>
-    <Tariff name={tariff.name} price={tariff.price} mgb={tariff.mgb} isHighlight={tariff.isHighlight}></Tariff>)
-    }
+        Tariffs.map((tariff, i) =>
+          <Tariff
+            key={tariff.price}
+            name={tariff.name}
+            price={tariff.price}
+            mgb={tariff.mgb} 
+            isHighlight={tariff.isHighlight} 
+            theme={themes[i]} 
+          />
+        )
+      }
     </div>
   );
 }
--- react3_tariff/src/components/Tariff.jsx
+++ react3_tariff/src/components/Tariff.jsx
@@ -1,15 +1,21 @@
 import styles from "./Tariff.module.scss";
+import * as cx from "classnames";
 
 function Tariff(props) {
+  const classTariff = cx(styles.Tariff, {
+    [styles.Tariff_highlight]: props.isHighlight,
+  });
+
   return (
-    <div
-      className={`${styles.Tariff} ${
-        props.isHighlight && styles.Tariff_highlight
-      }`}>
-      <div className={styles.Tariff__header}>
+    <div className={classTariff}>
+      <div className={cx(styles.Tariff__header, props.theme.Tariff__header)}>
         <h3>{props.name}</h3>
       </div>
-      <div className={styles.Tariff__priceWrapper}>
+      <div
+        className={cx(
+          styles.Tariff__priceWrapper,
+          props.theme.Tariff__priceWrapper
+        )}>
         <sup className={styles.Tariff__currency}>руб</sup>
         <h1 className={styles.Tariff__price}>{props.price}</h1>
         <span className={styles.Tariff__month}>/мес</span>
